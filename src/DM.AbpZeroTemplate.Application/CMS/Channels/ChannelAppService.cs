b9f447acff0f1f0914af4da4060e27cc1ea25183
sessionliang <sessionliang@outlook.com>
2016-04-19 23:30:57 +0800

    完善app和栏目管理

---
+++ src/DM.AbpZeroTemplate.Application/CMS/Channels/ChannelAppService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using DM.AbpZeroTemplate.CMS.Channels.Dto;
+using Abp.Authorization;
+using DM.AbpZeroTemplate.Authorization;
+using Abp.Channels;
+
+namespace DM.AbpZeroTemplate.CMS.Channels
+{
+    [AbpAuthorize(AppPermissions.Pages_CMS_Channels)]
+    public class ChannelAppService : AbpZeroTemplateAppServiceBase, IChannelAppService
+    {
+        private readonly ChannelManager _channelManager;
+
+        public Task<ChannelDto> CreateChannel(CreateChannelInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteChannel(IdInput<long> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ListResultOutput<ChannelDto>> GetChannels(IdInput<long> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ChannelDto> MoveChannel(MoveChannelInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ChannelDto> UpdateChannel(UpdateChannelInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DM.AbpZeroTemplate.Application/CMS/Channels/IChannelAppService.cs
@@ -0,0 +1,50 @@
+﻿using Abp.Application.Services.Dto;
+using DM.AbpZeroTemplate.CMS.Channels.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.AbpZeroTemplate.CMS.Channels
+{
+    /// <summary>
+    ///  栏目的领域服务
+    /// </summary>
+    public interface IChannelAppService
+    {
+        /// <summary>
+        /// 获取栏目
+        /// </summary>
+        /// <returns></returns>
+        Task<ListResultOutput<ChannelDto>> GetChannels(IdInput<long> input);
+
+        /// <summary>
+        /// 创建栏目
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        Task<ChannelDto> CreateChannel(CreateChannelInput input);
+
+        /// <summary>
+        /// 更新栏目
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        Task<ChannelDto> UpdateChannel(UpdateChannelInput input);
+
+        /// <summary>
+        /// 移动栏目
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        Task<ChannelDto> MoveChannel(MoveChannelInput input);
+
+        /// <summary>
+        /// 删除栏目
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        Task DeleteChannel(IdInput<long> input);
+    }
+}
